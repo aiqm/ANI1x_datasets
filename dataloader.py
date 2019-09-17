8a5b3337e47c26f90fb3ce1d14d42b04ab23dd6a
zubatyuk <zubatyuk@gmail.com>
2019-09-17 15:13:00 -0400

    Create dataloader.py

---
+++ dataloader.py
@@ -0,0 +1,26 @@
+import h5py
+import numpy as np
+
+
+def iter_data_buckets(h5filename, keys=['wb97x_dz.energy']):
+    """ Iterate over buckets of data in ANI HDF5 file. 
+    Yields dicts with atomic numbers (shape [Na,]) coordinated (shape [Nc, Na, 3])
+    and other available properties specified by `keys` list, w/o NaN values.
+    """
+    keys = set(keys)
+    keys.discard('atomic_numbers')
+    keys.discard('coordinates')
+    with h5py.File(h5filename, 'r') as f:
+        for grp in f.values():
+            Nc = grp['coordinates'].shape[0]
+            mask = np.ones(Nc, dtype=np.bool)
+            if not np.sum(mask):
+                continue
+            data = dict((k, grp[k][()]) for k in keys)
+            for k in keys:
+                v = data[k].reshape(Nc, -1)
+                mask = mask & ~np.isnan(v).any(axis=1)
+            d = dict((k, data[k][mask]) for k in keys)
+            d['atomic_numbers'] = grp['atomic_numbers'][()]
+            d['coordinates'] = grp['coordinates'][()][mask]
+            yield d 
